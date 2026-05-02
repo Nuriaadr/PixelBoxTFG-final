f0e686d7a4add085bd6fbe2b40363285bb51622b
Nuria <nuriadr2212@gmail.com>
2026-05-02 09:52:31 +0200

    jiju

--- docs/js/config.js
+++ docs/js/config.js
@@ -2,4 +2,4 @@ const API_URL =
   window.location.hostname === "127.0.0.1" ||
   window.location.hostname === "localhost"
     ? "http://localhost/PixelBoxTFGPruebas/backend/public"
-    : "https://tu-app.railway.app";
+    : "la url de tu back en produccion to wapo";
