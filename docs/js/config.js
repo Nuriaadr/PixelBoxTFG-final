5fe9666ec28993010d7e7ae889fbdafb9549921f
Nuria <nuriadr2212@gmail.com>
2026-05-02 09:37:34 +0200

    a

--- docs/js/config.js
+++
@@ -1,5 +0,0 @@
-const API_URL =
-  window.location.hostname === "127.0.0.1" ||
-  window.location.hostname === "localhost"
-    ? "http://localhost/PixelBoxTFGPruebas/backend/public"
-    : "https://tu-app.railway.app";
